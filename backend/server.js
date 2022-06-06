1a6e7d02fb13d66f6d8549aa6f1daccd5af25a6a
minwoohome <minwoohome2018@gmail.com>
2022-06-06 17:15:54 +0900

    adfasf

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { pool } = require("./db");
+
+pool.query(
+  `CREATE TABLE lists (
+  id INTEGER AUTO_INCREMENT,
+  value TEXT,
+  PRIMARY KEY (id)
+)`,
+  (err, results, fields) => {
+    console.log("results", results);
+  }
+);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/values", (req, res, next) => {
+  console.log(req);
+  pool.query("SELECT * FROM lists", (err, results, fields) => {
+    if (err) {
+      return res.status(500).send(err);
+    } else {
+      return res.json(results);
+    }
+  });
+});
+
+app.post("/api/values", (req, res, next) => {
+  console.log(req.body);
+  pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`, (err, results, fields) => {
+    if (err) {
+      return res.status(500).send(err);
+    } else {
+      return res.json({ success: true, value: req.body.value });
+    }
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running in port 5000");
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/api/values", {
+      method: "post",
+      body: JSON.stringify({
+        value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        setList((prev) => [value, ...prev]);
+        setValue("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetch("/api/values")
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        setList(data.map((d) => d.value));
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="텍스트 입력하세요" onChange={handleChange} value={value} />
+          <button>저장</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
